d98880bff89d1407b246d1d7519ea67a3841e1c9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-14 19:27:13 +0900

    [add] ViewBindingを追加

--- app/src/main/java/com/wsr/api_checker/MainActivity.kt
+++ app/src/main/java/com/wsr/api_checker/MainActivity.kt
@@ -2,10 +2,22 @@ package com.wsr.api_checker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wsr.api_checker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
+
+        binding.button.setOnClickListener {
+            binding.text.text = binding.urlInput.text
+        }
     }
 }
