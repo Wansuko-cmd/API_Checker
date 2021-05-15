2b3a1e6c55ac03ee0079574a2414d068a855fe2b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-15 23:36:51 +0900

    [add] Getメソッドを取る処理を追加

--- app/src/main/java/com/wsr/api_checker/MainActivity.kt
+++ app/src/main/java/com/wsr/api_checker/MainActivity.kt
@@ -3,6 +3,11 @@ package com.wsr.api_checker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wsr.api_checker.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +22,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.button.setOnClickListener {
-            binding.text.text = binding.urlInput.text
+            runBlocking {
+
+//                "https://i10jan-api.herokuapp.com/v1.1/api"
+                binding.text.text = okHttpGet(binding.urlInput.text.toString())
+            }
         }
+    }
+
+    private suspend fun okHttpGet(url: String) = withContext(Dispatchers.IO){
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient.Builder().build()
+
+        val response = client.newCall(request).execute()
+
+        return@withContext response.body?.string() ?: ""
     }
 }
