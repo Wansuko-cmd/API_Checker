3c326a234806f0ac83f1c5ae65a3ab7b56f93518
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-20 13:28:56 +0900

    [add] fragmentを作成

--- app/src/main/java/com/wsr/api_checker/MainActivity.kt
+++ app/src/main/java/com/wsr/api_checker/MainActivity.kt
@@ -2,51 +2,13 @@ package com.wsr.api_checker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import com.wsr.api_checker.databinding.ActivityMainBinding
-import com.wsr.api_checker.methods.Get
-import com.wsr.api_checker.methods.HttpMethod
-import com.wsr.api_checker.methods.Post
-import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var methods: HttpMethod
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        _binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            setContentView(this.root)
-        }
-
-        binding.run{
-
-            ArrayAdapter
-                .createFromResource(this@MainActivity, R.array.methods, android.R.layout.simple_spinner_item)
-                .apply {
-                setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = this
-            }
-
-            button.setOnClickListener {
-
-                val client = OkHttpClient.Builder().build()
-
-                when(spinner.selectedItem){
-                    "GET" -> methods = Get(client)
-                    "POST" -> methods = Post(client)
-                    "PUT" -> TODO()
-                    "DELETE" -> TODO()
-                }
-
-                runBlocking {
-                    binding.text.text = methods.getRequest(binding.urlInput.text.toString())
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
+
+//                "https://i10jan-api.herokuapp.com/v1.1/api"
---
+++ app/src/main/java/com/wsr/api_checker/fragments/InputUrlFragment.kt
@@ -0,0 +1,62 @@
+package com.wsr.api_checker.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.wsr.api_checker.R
+import com.wsr.api_checker.databinding.FragmentInputUrlBinding
+import com.wsr.api_checker.methods.Get
+import com.wsr.api_checker.methods.HttpMethod
+import com.wsr.api_checker.methods.Post
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+
+class InputUrlFragment : Fragment() {
+
+    private var _binding: FragmentInputUrlBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var methods: HttpMethod
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInputUrlBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.run{
+
+            ArrayAdapter
+                .createFromResource(requireContext(), R.array.methods, android.R.layout.simple_spinner_item)
+                .apply {
+                    setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    spinner.adapter = this
+                }
+
+            button.setOnClickListener {
+
+                val client = OkHttpClient.Builder().build()
+
+                when(spinner.selectedItem){
+                    "GET" -> methods = Get(client)
+                    "POST" -> methods = Post(client)
+                    "PUT" -> TODO()
+                    "DELETE" -> TODO()
+                }
+
+                runBlocking {
+                    binding.text.text = methods.getRequest(binding.urlInput.text.toString())
+                }
+            }
+        }
+    }
+}
